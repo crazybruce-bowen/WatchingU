155089dc7d78a4d3f44d11f6021c70fe67b1f816
Bruce <fake2.qq.com>
2022-12-04 12:18:27 +0800

    小的格式调整

--- code/core/core_catching.py
+++ code/core/core_catching.py
@@ -12,13 +12,15 @@ from typing import List
 now_dir = os.path.dirname(__file__)
 path_code = os.path.dirname(now_dir)
 path_root = os.path.dirname(path_code)
-if path_root not in sys.path:
-    sys.path.append(path_root)
+path_proj = os.path.dirname(path_root)
+for path in [path_root, path_proj]:
+    if path not in sys.path:
+        sys.path.append(path)
 
-from utils.common_utils import print_time, G_LJ, G_ZR
-from utils.html_service import get_one_page_html
-from utils.io_service import save_info_to_local, save_info_to_mongodb
-from utils.ocr_service import PricePredict
+from code.utils.common_utils import print_time, G_LJ, G_ZR
+from code.utils.html_service import get_one_page_html
+from code.utils.io_service import save_info_to_local, save_info_to_mongodb
+from code.utils.ocr_service import PricePredict
 
 
 class RoomInfoCatching:
